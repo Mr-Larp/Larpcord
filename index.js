d4a9fe69891e56fe6a3674872f23fc730c9f1bfd
Mr-Larp <Mr-Larp@users.noreply.github.com>
2026-08-18 14:35:22 +0000

    deploy: 565cf7478d8070a22fbd093c5965c565bdd44a39

---
+++ index.js
@@ -0,0 +1,46 @@
+(function (exports) {
+
+    let unpatch;
+
+    const onLoad = () => {
+        const { findByProps } = vendetta.metro;
+        const { after } = vendetta.patcher;
+
+        const extraPropsMod = findByProps("getExtraProperties");
+        if (extraPropsMod) {
+            unpatch = after("getExtraProperties", extraPropsMod, (_, res) => {
+                if (!res) return res;
+                res.os = "Windows";
+                res.browser = "Discord Client";
+                res.os_version = "10.0.19045";
+                res.browser_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Discord/1.0.9013 Chrome/120.0.6099.291 Electron/28.2.10 Safari/537.36";
+                res.client_build_number = 300000;
+                return res;
+            });
+        }
+
+        const socketMod = findByProps("getSocket");
+        const socket = socketMod?.getSocket();
+        if (socket) {
+            socket.sessionId = null;
+            socket.close(4000, "PlatformSpoofer reconnect");
+        }
+    };
+
+    const onUnload = () => {
+        if (unpatch) unpatch();
+        
+        const socketMod = vendetta?.metro?.findByProps("getSocket");
+        const socket = socketMod?.getSocket();
+        if (socket) {
+            socket.sessionId = null;
+            socket.close(4000, "PlatformSpoofer reconnect");
+        }
+    };
+
+    exports.onLoad = onLoad;
+    exports.onUnload = onUnload;
+
+    return exports;
+
+})({});
